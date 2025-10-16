8e15f612a69949b0ff4c0f937609da3628fefaba
Lyvan1 <galondelyvan@gmail.com>
2025-10-16 11:20:03 +0200

    Création de l'app users (gestion des utilisateurs). Ajout de la class User (first_name, last_name, email(unique), password (avec set_password & check_password pouru n mot de passe haché)

---
+++ weeb_api/users/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+# Create your models here.
+class User(models.Model):
+    """_User Entity_
+
+    Args:
+        first_name (str): user's firstname
+        last_name (str): user's lastname
+        email (str): user's email --> unique
+        password (str)
+    """
+    
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.EmailField(unique = True)
+    password = models.CharField(min_length = 6, ax_length = 128) #128 --> because of password hash
+    
+    def __str__(self):
+        return self.email
+    
+    def set_password(self, raw_password):
+        """
+        Hash password and save it to the object.
+
+        Args:
+            raw_password (str): password not hashed.
+        """
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        """
+        check if the given apssword is the same as the hashed password.
+
+        Args:
+            raw_password (str): password not hashed to check.
+
+        Returns:
+            bool: True if password corred, else False.
+        """
+        return check_password(raw_password, self.password)

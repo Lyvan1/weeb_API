cfd45b14f00569b111dc233bd9317440126ac341
Lyvan1 <galondelyvan@gmail.com>
2025-10-17 13:13:04 +0200

    nothing

--- weeb_api/users/views.py
+++ weeb_api/users/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from .serializers import UserSerializer
 from django.http import Http404
 
-# Create your views here.
+# Create your views here
 class UserViewSet(viewsets.ModelViewSet):
     """
     __ViewSet to handle Users__
